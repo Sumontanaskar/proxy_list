c2d1ee8b1655424b9461620878341873dac06874
root <newsumonts@gmail.com>
2017-08-28 17:39:02 +0800

    02 28-08-2017

--- Proxy_list.py
+++ Proxy_list.py
@@ -1,6 +1,7 @@
 import urllib2
 from bs4 import BeautifulSoup
 import base64
+import os
 output_file = ''
 url = 'https://proxy-list.org/english/index.php'
 
@@ -31,8 +32,22 @@ for list in output:
         f_write('\n')
 
 
-file =open('p_list.txt', 'r')
+file =open('p_list.txt', "r")
+
+str='''
+dynamic_chain
+proxy_dns
+tcp_read_time_out 15000
+tcp_connect_time_out 8000
+[ProxyList]
+    '''
+f = open('proxychains.conf', "a+")
+f.write(str)
 for list in file:
-    data =  list.split(':')
-    k = data[2]+'\t'+data[0]+'\t'+data[1]
+    data = list.split(':')
+    k = data[2]+'\t'+data[0]+'\t'+data[1]+'\n'
+    f.write(k)
     print k
+f.close()
+file.close()
+os.remove('p_list.txt')
